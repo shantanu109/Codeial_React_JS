0c11bd705813885eeb4e366ad0cb0b238d8c0a91
shantanu pandey <shantanupandey@shantanus-MacBook.local>
2021-05-24 18:20:07 +0530

    Form handling Uncontrolled Components

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,22 +1,43 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
-    render() {
-        return (
-            <form className="login-form">
-                <span className="login-signup-header">Login</span>
-                <div className="field">
-                    <input type="email" placeholder="Email" required />
-                </div>
-                <div className="field">
-                    <input type="password" placeholder="Password" required />
-                </div>
-                <div className="field">
-                    <button >Log In</button>
-                </div>
-            </form>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.emailInputRef = React.createRef();
+    this.passwordInputRef = React.createRef();
+  }
+
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+    console.log('this.emailInputRef', this.emailInputRef);
+    console.log('this.passwordInputRef', this.passwordInputRef);
+  };
+  render() {
+    return (
+      <form className="login-form">
+        <span className="login-signup-header">Login</span>
+        <div className="field">
+          <input
+            type="email"
+            placeholder="Email"
+            required
+            ref={this.emailInputRef}
+          />
+        </div>
+        <div className="field">
+          <input
+            type="password"
+            placeholder="Password"
+            required
+            ref={this.passwordInputRef}
+          />
+        </div>
+        <div className="field">
+          <button onClick={this.handleFormSubmit}>Log In</button>
+        </div>
+      </form>
+    );
+  }
 }
 
 export default Login;
