c1e6f9eb3080ddca05d81f91537d03024b4098f5
shantanu pandey <shantanupandey@shantanus-MacBook.local>
2021-05-26 22:52:03 +0530

    Added Private Route for Settings

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,35 @@
-
 import React from 'react';
-import {connect} from 'react-redux';
-import {fetchPosts} from '../actions/posts';
-import { Navbar, Home, Page404, Login} from './';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/posts';
+import { Navbar, Home, Page404, Login } from './';
 import PropTypes from 'prop-types';
-import { BrowserRouter as Router, Link, Route,Switch } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Link,
+  Redirect,
+  Route,
+  Switch,
+} from 'react-router-dom';
 import Signup from '../helpers/Signup';
 import jwtDecode from 'jwt-decode';
 import { authenticateUser } from '../actions/auth';
 
+const Settings = () => <div>Setting</div>;
 
+const PrivateRoute = (privateRouteProps) => {
+  const { isLoggedIn, path, component: Component } = privateRouteProps;
 
-
+  return (
+    <Route
+      path={path}
+      render={(props) => {
+        return isLoggedIn ? <Component {...props} /> : <Redirect to="/login" />;
+      }}
+    />
+  );
+};
 
 class App extends React.Component {
-
   componentDidMount() {
     //type an action that I want to fetch posts
 
@@ -22,51 +37,59 @@ class App extends React.Component {
 
     const token = localStorage.getItem('token');
 
-    if (token){
+    if (token) {
       const user = jwtDecode(token);
 
-      console.log('user',user);
+      console.log('user', user);
 
-      this.props.dispatch(authenticateUser({
-        email: user.email,
-        _id: user._id,
-        name: user.name
-
-      }));
+      this.props.dispatch(
+        authenticateUser({
+          email: user.email,
+          _id: user._id,
+          name: user.name,
+        })
+      );
     }
-    
   }
-  
+
   render() {
-    const {posts} = this.props;
+    const { posts, auth } = this.props;
     return (
       <Router>
         <div>
-        <Navbar />
-      <Switch>
-        <Route exact={true} path="/" render={(props) => {
-          return <Home {...props} posts={posts}/>
-        }}/>
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={Signup} />
-        <Route component={Page404}/>
-      </Switch>
-
+          <Navbar />
+          <Switch>
+            <Route
+              exact={true}
+              path="/"
+              render={(props) => {
+                return <Home {...props} posts={posts} />;
+              }}
+            />
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={Signup} />
+            <PrivateRoute
+              path= "/settings"
+              component={Settings}
+              isLoggedIn={auth.isLoggedIn}
+            />
+            <Route component={Page404} />
+          </Switch>
         </div>
       </Router>
-      
     );
   }
 }
 
-function mapStateToProps (state){
-  return{
-    posts: state.posts
-  }
+function mapStateToProps(state) {
+  return {
+    posts: state.posts,
+    auth: state.auth,
+  };
 }
 
 App.propTypes = {
-  posts: PropTypes.array.isRequired
-}
+  posts: PropTypes.array.isRequired,
+};
 
 export default connect(mapStateToProps)(App);
